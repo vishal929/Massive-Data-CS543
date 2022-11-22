f47535354d01da7314e2757b71dcb4789d01fa50
vishal929 <48373882+vishal929@users.noreply.github.com>
2022-11-22 12:52:37 -0500

    decrease batch size

--- deep_learning/train.py
+++ deep_learning/train.py
@@ -118,7 +118,7 @@ learning_rate = 0.3
 num_hidden = 3
 num_hidden_features = 30
 input_features = 15
-batch_size = 8388608
+batch_size = 4194304
 
 num_epochs_total = 100
 num_epoch_save_interval = 1
