e1abd38b90a0e2c2c3744632bd5e56bb643ab36a
vishal929 <48373882+vishal929@users.noreply.github.com>
2022-12-05 00:27:35 -0500

    fix pandas

--- deep_learning/test.py
+++ deep_learning/test.py
@@ -77,11 +77,11 @@ def get_attributes_for_regression(guesses,actual):
     # getting within 5 minute accuracy
     data['5-min-bool'] = abs(data['predicted']-data['actual']) <= 5
     #data['5-min-bool'] = data.apply(lambda x: x['actual']-5 <= x['predicted'] <= x['actual']+5)
-    print('model 5-min accuracy: ' + str(data['5-min-bool'].sum()/data.size()))
+    print('model 5-min accuracy: ' + str(data['5-min-bool'].sum()/data.shape[0]))
     # getting within 10 minute accuracy
     data['10-min-bool'] = abs(data['predicted'] - data['actual']) <= 10
     #data['10-min-bool'] = data.apply(lambda x: x['actual'] - 10 <= x['predicted'] <= x['actual']+10)
-    print('model 10-min accuracy: ' + str(data['10-min-bool'].sum() / data.size()))
+    print('model 10-min accuracy: ' + str(data['10-min-bool'].sum() / data.shape[0]))
 
 # getting data loader for test data
 batch_size = 524288
